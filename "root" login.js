8f32a6e1608c58d6958926abe8a706c346221fb7
Aleksey Samusenko <aleksej.samusenko93@gmail.com>
2017-01-16 00:25:02 +0200

    finish logic part of code

--- "root" login.js
+++ "root" login.js
@@ -1,39 +1,63 @@
-/* This simple Javascript code is created to set automatically "root" as a login
-Created by Aleksey Samusenko8*/
 
-
-
-(function () {
-    
-    var buttonHTML = "<input type=\"button\" value=\"Set 'root' login\" >",
-        formBlock = document.querySelector("form");
+    var changeLoginButton = "<input type=\"button\" value=\"Change\" id=\"change\" >",
+        returnButton = "<input type=\"button\" value=\"Return\" id=\"return\" >",
+        inputField = "<input type=\"text\" value=\"root\" id=\"inputValue\" >",
+        formBlock = document.querySelector("form"),
+        divBlock = "<div id=\"insertedBlock\"></div>";
 
 //Creating a button
 
-    formBlock.insertAdjacentHTML('beforeend',buttonHTML);   
+    formBlock.insertAdjacentHTML('afterend', divBlock);
+
+    var divBlocHtml = document.getElementById('insertedBlock');
+
+    divBlocHtml.insertAdjacentHTML('beforeend', changeLoginButton);
+    divBlocHtml.insertAdjacentHTML('beforeend', inputField);
+    divBlocHtml.insertAdjacentHTML('beforeend', returnButton);
+
+//Changing CSS property
+    
+    var inputFieldHtml = document.getElementById("inputValue");
+    inputFieldHtml.style.font_size = "";
 
 //Creating event
-
-    formBlock.addEventListener('click', replaceLogin);
+    
+    divBlocHtml.addEventListener('click', clicking, false);
 
 //Replacing function
 
-    function replaceLogin(){
-        var tunnelDetails = document.querySelectorAll('td.tunnel_details'),
-            login = "root";
-       
-     
-        for(var i = 0, arrLength = tunnelDetails.length ; i < arrLength ; i++ ){
-            var currentTunnelDetails = tunnelDetails[i].innerText,
-                firstPart = currentTunnelDetails.substr(0, currentTunnelDetails.indexOf("l") + 1),
-                lastPart = currentTunnelDetails.substr(currentTunnelDetails.lastIndexOf("-"), currentTunnelDetails.length),
-                finalTunnelDetails = firstPart + " " + login + " " + lastPart;
-            
-            //setting "root" as as login
+    function clicking (e){
+        var target = e && e.target || e.srcElement,
+            buttonId = target.id;
 
-                tunnelDetails[i].innerText = finalTunnelDetails;
+
+        if(buttonId == "change"){
+            var inputValue = document.querySelector('#inputValue').value;
+            replaceLogin(inputValue);
+        }else if(buttonId == "return"){
+            replaceLogin("CAMERA_USERNAME");
+        }
+
+    };
+
+    function replaceLogin(login){
+        var tunnelDetails = document.querySelectorAll('td.tunnel_details'),
+            loginDetails = login;
+
+       
+        
+        for(var i = 0, arrLength = tunnelDetails.length ; i < arrLength ; i++ ){
+
+            var currentTunnelDetails = tunnelDetails[i].innerText;
+                // current Tunnel Details in iDashboard
+                if(currentTunnelDetails.slice(0, 3) == "ssh"){
+                    var firstPart = currentTunnelDetails.substr(0, currentTunnelDetails.indexOf("l") + 1),
+                    lastPart = currentTunnelDetails.substr(currentTunnelDetails.lastIndexOf("-"), currentTunnelDetails.length),
+                    finalTunnelDetails = firstPart + " " + loginDetails + " " + lastPart;
+                    //setting "root" as as login
+                    tunnelDetails[i].innerText = finalTunnelDetails;
+
+                };       
+
         };
     };
-       
-} )();
-
